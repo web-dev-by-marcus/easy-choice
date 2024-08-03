3d8a426f42b74d8c4233d27f7f535d07b806f4fe
marcuspramirez <@macusramirez@gmail.com>
2024-08-03 16:14:13 -0500

    get user dynamically

--- easy-choice/src/app/user/user.component.ts
+++ easy-choice/src/app/user/user.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users.ts';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
