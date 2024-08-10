0ac4d57cf1c9e6b895161586c61e05254071412c
marcuspramirez <@macusramirez@gmail.com>
2024-08-09 22:40:50 -0500

    add event emmitier

--- easy-choice/src/app/user/user.component.ts
+++ easy-choice/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,16 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectedUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
