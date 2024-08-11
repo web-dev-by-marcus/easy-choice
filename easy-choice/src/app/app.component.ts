f1985a9ad7ab2598911fbcdb29deced22c11f2d6
marcuspramirez <@macusramirez@gmail.com>
2024-08-10 19:51:25 -0500

    add tasks component to output selected user

--- easy-choice/src/app/app.component.ts
+++ easy-choice/src/app/app.component.ts
@@ -2,18 +2,28 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users.ts';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log(`selected user with id ${id}`);
+    return (this.selectedUserId = id);
   }
+
+  // selectTask(name: string) {
+  //   console.log(`the name clicked is ${name}`);
+  // }
 }
