19208d005ade445fbffdc9b83998d6f101a2cf2b
marcuspramirez <@macusramirez@gmail.com>
2024-08-14 16:45:46 -0500

    add interface to seperate file

--- easy-choice/src/app/user/user.component.ts
+++ easy-choice/src/app/user/user.component.ts
@@ -1,16 +1,5 @@
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// };
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { Component, Input, output } from '@angular/core';
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
