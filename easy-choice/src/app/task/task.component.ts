c328a47c94838377b1553a33f3c60f99b992e69e
marcuspramirez <@macusramirez@gmail.com>
2024-08-14 12:52:40 -0500

    take an input to output task title, date and summary

--- easy-choice/src/app/task/task.component.ts
+++ easy-choice/src/app/task/task.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-
+  @Input({ required: true }) task!: task;
 }
