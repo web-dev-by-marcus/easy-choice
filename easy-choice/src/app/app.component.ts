a230a9cb3d7faf352bae5f2e3cc7b62cfcb6ada9
marcuspramirez <@macusramirez@gmail.com>
2024-08-18 08:27:14 -0500

    highlight selected user by matching selected ID conditionally

--- easy-choice/src/app/app.component.ts
+++ easy-choice/src/app/app.component.ts
@@ -3,17 +3,20 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users.ts';
 import { TasksComponent } from './tasks/tasks.component';
+import { FooterComponent } from './footer/footer.component';
+import { DUMMY_FOOTER_DATA } from './dummy-footer-data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string;
+  footerData = DUMMY_FOOTER_DATA;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
--- easy-choice/src/app/user/user.component.ts
+++ easy-choice/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, output } from '@angular/core';
+import { Component, input, Input, output } from '@angular/core';
 import { type User } from './user.model';
 
 @Component({
@@ -10,9 +10,9 @@ import { type User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-
   // @Output() select = new EventEmitter();
   select = output<string>();
+  @Input({ required: true }) selected!: boolean;
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
