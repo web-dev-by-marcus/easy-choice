962979a5de31c29db68ca2770f850b12b5526702
marcuspramirez <@macusramirez@gmail.com>
2024-09-13 23:42:08 -0500

    created shared css and use ng-content to wrap and include content

--- easy-choice/src/app/user/user.component.ts
+++ easy-choice/src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, Input, output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
