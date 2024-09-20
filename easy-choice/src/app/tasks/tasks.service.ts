ac6dc245fff26406ee010fb37eb0752cc7ee29bc
marcuspramirez <@macusramirez@gmail.com>
2024-09-19 22:28:08 -0500

    add local storage not working

--- easy-choice/src/app/tasks/tasks.service.ts
+++ easy-choice/src/app/tasks/tasks.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { NewTaskData } from '../task/task.model';
 
-
 @Injectable({ providedIn: 'root' })
 export class TasksService {
   private tasks = [
@@ -30,6 +29,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -42,9 +49,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks;
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks;
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
